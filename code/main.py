a2104d5688cff6ea22e4187b3935328cc01a63de
XdragonianX <158476287+XdragonianX@users.noreply.github.com>
2026-09-11 19:33:07 +0300

    Add files via upload

---
+++ code/main.py
@@ -0,0 +1,39 @@
+import os
+
+
+
+
+running = True
+
+
+class Main:
+    def __init__(self):
+        pass
+
+
+
+
+    def loop():
+        print("hi")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main = Main() # run init
+
+    while running:
+        Main.loop() # run loop
